323e84c197e804892a72b5b90eecf8aaea639a72
novaniindra <55472919+novaniindra@users.noreply.github.com>
2020-03-21 21:48:22 +0700

    Update soal5.py

--- soal5.py
+++ soal5.py
@@ -2,13 +2,16 @@ JAWABAN NOMER 5
 ==================================================
 def groupNumber():
     import numpy as np
-    symbols = " "
+    symbols = " -"
     sentence = "993141 -1 1323 14-232"
-
     for i in symbols:
         #print(sentence)
         sentence = np.char.replace(sentence, i, '')
-
     print(sentence)
+    angka=[0]
+    while angka>=50:
+        if (angka%3)==0:
+            angka=sentence
+        print(sentence)
 groupNumber()
 ===================================================
